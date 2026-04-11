b6706c9506c00f46aa834343d66d56e2eeb3f4d6
Mostafa1712002 <mostafaibrahim1712002@gmail.com>
2026-04-11 20:49:12 +0200

    Fix QR service for chillerlan/php-qrcode v6 API

--- app/Services/ZatcaQrService.php
+++ app/Services/ZatcaQrService.php
@@ -4,38 +4,18 @@ namespace App\Services;
 
 use chillerlan\QRCode\QRCode;
 use chillerlan\QRCode\QROptions;
+use chillerlan\QRCode\Output\QRGdImagePNG;
 
 class ZatcaQrService
 {
     public function generateBase64Image(string $tlvBase64): string
     {
-        $options = new QROptions([
-            'outputType' => QRCode::OUTPUT_IMAGE_PNG,
-            'eccLevel' => QRCode::ECC_L,
-            'scale' => 5,
-            'imageBase64' => false,
-        ]);
+        $options = new QROptions();
+        $options->outputInterface = QRGdImagePNG::class;
+        $options->scale = 5;
 
         $qr = new QRCode($options);
-        $imageData = $qr->render($tlvBase64);
 
-        return 'data:image/png;base64,' . base64_encode($imageData);
-    }
-
-    public function generateFile(string $tlvBase64, string $path): string
-    {
-        $options = new QROptions([
-            'outputType' => QRCode::OUTPUT_IMAGE_PNG,
-            'eccLevel' => QRCode::ECC_L,
-            'scale' => 5,
-            'imageBase64' => false,
-        ]);
-
-        $qr = new QRCode($options);
-        $imageData = $qr->render($tlvBase64);
-
-        file_put_contents($path, $imageData);
-
-        return $path;
+        return $qr->render($tlvBase64);
     }
 }
